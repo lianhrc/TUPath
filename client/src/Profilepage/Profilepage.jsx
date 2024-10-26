b77df4781c6ce07bc38e04347d4cc112d9da28b1
lianhrc <androderrick@gmail.com>
2024-10-26 20:01:10 +0800

    profilepage UI

--- client/src/Profilepage/Profilepage.jsx
+++ client/src/Profilepage/Profilepage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderHomepage from '../components/headerhomepage';
 import MessagePop from '../components/messagingpop';
 import './Profilepage.css';
-import avatar from '../assets/profile.png';
+import avatar from '../assets/profileicon.png';
 import location from '../assets/location.png';
 import since from '../assets/since.png';
 import projectThumbnail from '../assets/joinTUP.jpg';
@@ -32,27 +32,39 @@ function Profilepage() {
             
           </div>
          
-          <div className="profile-section">
-            <h3>Description</h3>
-            <p>Hello! I am passionate in doing great project like video editing, designs for brochures, YouTube thumbnails and cartoon portraits. In past 3 years I do several works that depends on the clients on what they want.</p>
-            <a href="#">Edit Description</a>
+         <div className='profile-main'>
+            <div className="profile-section">
+                <div className='profilesectiontop'>
+                  <h3>Description</h3>
+                  <a href="#">Edit Description</a>
+                </div>
+                <p>Hello! I am passionate in doing great project like video editing, designs for brochures, YouTube thumbnails and cartoon portraits. In past 3 years I do several works that depends on the clients on what they want.</p>
+              </div>
+              <div className="profile-section">
+                <div className='profilesectiontop'>
+                  <h3>Skills</h3>
+                  <a href="#">Add New</a>
+                </div>
+                <p>Front-end Developer</p>
+              </div>
+              <div className="profile-section">
+
+                <div className='profilesectiontop'>
+                  <h3>Certificates / Achievements</h3>
+                  <a href="#">Add New</a>
+                  </div>
+                  <p>N/A</p>
+               
+              </div>
+            </div>
           </div>
-          <div className="profile-section">
-            <h3>Skills</h3>
-            <p>Front-end Developer</p>
-            <a href="#">Add New</a>
-          </div>
-          <div className="profile-section">
-            <h3>Certificates / Achievements</h3>
-            <p>N/A</p>
-            <a href="#">Add New</a>
-          </div>
-        </div>
 
         <div className="projects-section">
           <h3>My Projects</h3>
+          <hr></hr>
           <div className="projects-grid">
             <div className="project-card add-project">
+              <p>+</p>
               <p>Add a Project</p>
             </div>
             <div className="project-card">
