23e2bb45175b9ecf0066fa05cc8ca0444bde7289
lianhrc <androderrick@gmail.com>
2024-10-26 22:52:51 +0800

    adding dropdown for icons

--- client/src/Profilepage/EmployeeProfileCreation.jsx
+++ client/src/Profilepage/EmployeeProfileCreation.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../Profilepage/EmployeeProfileCreation.css'
+import logo from '../assets/logoicon.png';
 
 function EmployeeProfileCreation() {
   return (
-    <div>
-      <h1>this is Employee Profile page</h1>
+    <div className="profile-container">
+      <div className="profile-header">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="profile-content">
+        <div className="profile-picture">
+          <button><div className="picture-placeholder">+</div></button>
+            
+        </div>
+        <form className="profile-form">
+          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Student ID" />
+          <select>
+            <option>Information Technology</option>
+            <option>Computer Science</option>
+            <option>Information System</option>
+            {/* Add more options as needed */}
+          </select>
+          <input type="text" placeholder="Year Level" />
+          <textarea placeholder="Bio / About me"></textarea>
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Contact" />
+        </form>
+        </div>
+        <div className="profilecreation-btn">
+          <button type="submit" className="submit-button">Submit</button>
+        </div>
     </div>
   )
 }
