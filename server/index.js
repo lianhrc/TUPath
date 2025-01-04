2b124deb67452c22054835c32f3d801bd3c23673
lianhrc <androderrick@gmail.com>
2025-01-04 23:25:48 +0800

    turned cloud

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const express = require("express");
   const crypto = require("crypto");
   
 
- //require('dotenv').config()
+ require('dotenv').config()
 
 
   const adminsignup = require("./routes/adminsignup");
@@ -62,22 +62,21 @@ const express = require("express");
   });
 
 
-  // MongoDB connection
-   mongoose
-     .connect("mongodb://127.0.0.1:27017/tupath_users")
-     .then(() => console.log("MongoDB connected successfully"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+  // // MongoDB connection
+  //  mongoose
+  //    .connect("mongodb://127.0.0.1:27017/tupath_users")
+  //    .then(() => console.log("MongoDB connected successfully"))
+  //   .catch((err) => console.error("MongoDB connection error:", err));
  
 
-/*
-// MongoDB connection
+
 mongoose.connect(
   "mongodb+srv://ali123:ali123@cluster0.wfrb9.mongodb.net/tupath_users?retryWrites=true&w=majority"
 )
   .then(() => console.log("Connected to MongoDB Atlas successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
- */
+ 
 
 
   // Configure multer for file uploads
